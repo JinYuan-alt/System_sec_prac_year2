0d185b239b5057b3fa648599d84e674217e46b1d
JinYuan-alt <232499R@mymail.nyp.edu.sg>
2024-08-11 21:17:57 +0800

    image sanitization

--- login.py
+++ login.py
@@ -320,44 +320,6 @@ def image():
         return render_template('post.html', filename=msg)
     return render_template('post.html')
 
-def check():
-    file_path= NonSan_filepath[-1]
-    image=Image.open(file_path)
-    exifdata = image.getexif()
-    info=IPTCInfo(file_path)
-    # looping through all the tags present in exifdata
-    for tagid in exifdata:
-        # getting the tag name instead of tag id
-        tagname = TAGS.get(tagid, tagid)
-
-        # passing the tagid to get its respective value
-        value = exifdata.get(tagid)
-
-        # printing the final result
-        print(f"{tagname:25}: {value}")
-    if info != None:
-        print(info)
-    else: print("nothing found")
-
-def presanitize():
-    file_path = NonSan_filepath[-1]
-    image = Image.open(file_path)
-    exifdata = image.getexif()
-    info = IPTCInfo(file_path)
-    xmpdata=image.getxmp()
-    # looping through all the tags present in exifdata
-    print(xmpdata)
-    print(info)
-    for tagid in exifdata:
-        # getting the tag name instead of tag id
-        tagname = TAGS.get(tagid, tagid)
-
-        # passing the tagid to get its respective value
-        value = exifdata.get(tagid)
-
-        # printing the final result
-        print(f"{tagname:25}: {value}")
-
 
 if __name__== '__main__':
     app.run()
--- main.py
+++ main.py
@@ -1,7 +1,46 @@
-from flask import Flask, render_template
+from PIL import Image
+import os
+from PIL.ExifTags import TAGS
+from iptcinfo3 import IPTCInfo
+import login
+def check():
+    file_path= ''
+    image=Image.open(file_path)
+    exifdata = image.getexif()
+    info=IPTCInfo(file_path)
+    # looping through all the tags present in exifdata
+    for tagid in exifdata:
+        # getting the tag name instead of tag id
+        tagname = TAGS.get(tagid, tagid)
 
-app=Flask(__name__)
+        # passing the tagid to get its respective value
+        value = exifdata.get(tagid)
 
-@app.route('/')
-def layout():
-    return render_template("index.html")
+        # printing the final result
+        print(f"{tagname:25}: {value}")
+    if info != None:
+        print(info)
+    else: print("nothing found")
+
+def presanitize():
+    file_path = ''
+    image = Image.open(file_path)
+    exifdata = image.getexif()
+    info = IPTCInfo(file_path)
+    xmpdata=image.getxmp()
+    # looping through all the tags present in exifdata
+    print(xmpdata)
+    print(info)
+    for tagid in exifdata:
+        # getting the tag name instead of tag id
+        tagname = TAGS.get(tagid, tagid)
+
+        # passing the tagid to get its respective value
+        value = exifdata.get(tagid)
+
+        # printing the final result
+        print(f"{tagname:25}: {value}")
+
+
+check()
+presanitize()
